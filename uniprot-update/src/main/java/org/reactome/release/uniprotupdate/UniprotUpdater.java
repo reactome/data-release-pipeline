d002f63dca459ec019cb9769cbaab143a169ae33
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2018-07-16 10:55:06 -0400

    More work on uniprot update

--- uniprot-update/src/main/java/org/reactome/release/uniprotupdate/UniprotUpdater.java
+++ uniprot-update/src/main/java/org/reactome/release/uniprotupdate/UniprotUpdater.java
@@ -1,7 +1,6 @@
 package org.reactome.release.uniprotupdate;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -23,12 +22,15 @@ import org.reactome.release.uniprotupdate.dataschema.UniprotData;
 public class UniprotUpdater
 {
 
+	private static final String ENSEMBL_HOMO_SAPIENS_GENE = "ENSEMBL_Homo_sapiens_GENE";
 	private static final String HOMO_SAPIENS = "Homo sapiens";
 	// List of species names was taken from uniprot_xml2sql_isoform.pl:84
 	private static final Set<String> speciesToUpdate = new HashSet<String>( Arrays.asList(HOMO_SAPIENS, "Mus musculus", "Rattus norvegicus",
 																			"Bos taurus", "Gallus gallus", "Drosophila melanogaster",
 																			"Caenorhabditis elegans", "Saccharomyces cerevisiae", "Schizosaccharomyces pombe",
 																			"Human immunodeficiency virus type 1", "Human immunodeficiency virus type 2", "Influenza A virus") );
+	
+	private static GKInstance ensemblHSapiensRefDB;
 	
 	private static String geneNamesListToString(List<GeneName> geneNames)
 	{
@@ -52,6 +54,16 @@ public class UniprotUpdater
 	 */
 	public void updateUniprotInstances(MySQLAdaptor adaptor, List<UniprotData> uniprotData, Map<String, GKInstance> referenceDNASequences, InstanceEdit instanceEdit) throws Exception
 	{
+		synchronized (UniprotUpdater.ensemblHSapiensRefDB)
+		{
+			// initialize if it's null.
+			if (UniprotUpdater.ensemblHSapiensRefDB == null)
+			{
+				List<GKInstance> refDBs = (List<GKInstance>)adaptor.fetchInstanceByAttribute(ReactomeJavaConstants.ReferenceDatabase, ReactomeJavaConstants.name, "=", ENSEMBL_HOMO_SAPIENS_GENE);
+				UniprotUpdater.ensemblHSapiensRefDB = refDBs.get(0);
+			}
+		}
+		
 		@SuppressWarnings("unchecked")
 		GKInstance humanSpecies = ((List<GKInstance>) adaptor.fetchInstanceByAttribute(ReactomeJavaConstants.species, ReactomeJavaConstants.name, "=", HOMO_SAPIENS)).get(0);
 		
@@ -81,25 +93,45 @@ public class UniprotUpdater
 								// If this instance already exists in the database, let's update it.
 								GKInstance referenceDNASequence = referenceDNASequences.get(geneNameFromFile);
 								GKInstance speciesFromDB = (GKInstance) referenceDNASequence.getAttributeValue(ReactomeJavaConstants.Species);
+								@SuppressWarnings("unchecked")
 								Set<String> speciesNamesFromDB = (Set<String>) speciesFromDB.getAttributeValuesList(ReactomeJavaConstants.name);
+								// The old Perl code forces the species to be changed if the one in the database does not match the one in the file.
 								if (!speciesNamesFromDB.contains(data.getScientificName()))
 								{
 									referenceDNASequence.setAttributeValue(ReactomeJavaConstants.species, humanSpecies);
+									adaptor.updateInstanceAttribute(referenceDNASequence, ReactomeJavaConstants.species);
+									modified = true;
 								}
 								
+								@SuppressWarnings("unchecked")
 								Set<String> geneNamesFromDB = (Set<String>)referenceDNASequence.getAttributeValuesList(ReactomeJavaConstants.geneName);
+								// The old Perl code adds the geneName from the file, if it's not already in the database.
 								if (!geneNamesFromDB.contains(geneNameFromFile))
 								{
-									modified = true;
 									referenceDNASequence.addAttributeValue(ReactomeJavaConstants.geneName, geneNameFromFile);
+									adaptor.updateInstanceAttribute(referenceDNASequence, ReactomeJavaConstants.geneName);
+									modified = true;
 								}
 								
+								// The old Perl code sets the reference database if it's not ENSEMBL_Homo_sapiens_GENE
+								if (!((String)((GKInstance)referenceDNASequence.getAttributeValue(ReactomeJavaConstants.referenceDatabase)).getAttributeValue(ReactomeJavaConstants.name)).equals(UniprotUpdater.ENSEMBL_HOMO_SAPIENS_GENE) )
+								{
+									referenceDNASequence.setAttributeValue(ReactomeJavaConstants.referenceDatabase, UniprotUpdater.ensemblHSapiensRefDB);
+									adaptor.updateInstanceAttribute(referenceDNASequence, ReactomeJavaConstants.referenceDatabase);
+									modified = true;
+								}
+								// if the instance was modified, attach a new InstanceEdit to the modified attribute.
 								if (modified)
 								{
+									referenceDNASequence.getAttributeValuesList(ReactomeJavaConstants.modified);
 									referenceDNASequence.addAttributeValue(ReactomeJavaConstants.modified, instanceEdit);
-									adaptor.updateInstanceAttribute(referenceDNASequence, ReactomeJavaConstants.geneName);
 									adaptor.updateInstanceAttribute(referenceDNASequence, ReactomeJavaConstants.modified);
 								}
+							}
+							// if the gene ID was NOT in the ReferenceDNASequences map, we may need to add it to the database.
+							else
+							{
+								GKInstance newRefDNASequence = new GKInstance(adaptor.getSchema().getClassByName(ReactomeJavaConstants.ReferenceDNASequence));
 							}
 						}
 					}

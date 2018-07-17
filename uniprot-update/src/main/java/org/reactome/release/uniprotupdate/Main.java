8a06265ac18429a34adb26778939e07d317993b8
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2018-07-17 17:11:52 -0400

    Fill in details for Main.

--- uniprot-update/src/main/java/org/reactome/release/uniprotupdate/Main.java
+++ uniprot-update/src/main/java/org/reactome/release/uniprotupdate/Main.java
@@ -1,14 +1,39 @@
 package org.reactome.release.uniprotupdate;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * 
  * @author sshorser
  *
  */
-public class Main {
+public class Main
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException, IOException
+	{
+		String fileName = "uniprot-update.properties";
+		
+		if (args.length > 0)
+		{
+			fileName = args[0];
+		}
+		
+		Properties props = new Properties();
+		props.load(new FileInputStream(fileName));
 
+		UniprotUpdateStep step = new UniprotUpdateStep();
+		try
+		{
+			step.executeStep(props);
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }

10843b204f7a490b2834099bf255f84f286d8105
Joel Weiser <joel.weiser@oicr.on.ca>
2019-01-14 18:33:44 -0500

    Refactors ProcessUniprotXML.java

--- uniprot-update/src/main/java/org/reactome/release/uniprotupdate/ProcessUniprotXML.java
+++ uniprot-update/src/main/java/org/reactome/release/uniprotupdate/ProcessUniprotXML.java
@@ -1,10 +1,6 @@
 package org.reactome.release.uniprotupdate;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +8,7 @@ import java.util.List;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
 import javax.xml.bind.util.JAXBResult;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
@@ -38,7 +35,12 @@ class ProcessUniprotXML
 {
 	private static final Logger logger = LogManager.getLogger();
 	private static final String ENTRY_ELEMENT_NAME = "entry";
+	// Starting size of ArrayList was determined by counting the number of "<entry>" lines in the input file.
+	// In fact, there were only about 530000 when I checked, but I figure it can't hurt to have a little extra room.
+	private static final int EXPECTED_ENTRY_MAX_SIZE = 600000;
 	private static final String PATH_TO_XSL = "src/main/resources/uniprotXMLProcessor.xsl";
+
+	private static OutputStream debugOutputStream;
 
 	/**
 	 * This function will process a Uniprot XML file, and return a list of all UniprotData objects that were extracted.
@@ -49,11 +51,10 @@ class ProcessUniprotXML
 	 * @throws TransformerConfigurationException
 	 * @throws FileNotFoundException
 	 */
-	public static List<UniprotData> getDataFromUniprotFile(String pathToFile, boolean debugXML) throws JAXBException, XMLStreamException, TransformerConfigurationException, FileNotFoundException
+	public static List<UniprotData> getDataFromUniprotFile(String pathToFile, boolean debugXML)
+		throws JAXBException, XMLStreamException, TransformerConfigurationException, FileNotFoundException
 	{
-		// Starting size of ArrayList was determined by counting the number of "<entry>" lines in the input file.
-		// In fact, there were only about 530000 when I checked, but I figure it can't hurt to have a little extra room.
-		List<UniprotData> uniprotData = new ArrayList<>(600000);
+		List<UniprotData> uniprotData = new ArrayList<>(EXPECTED_ENTRY_MAX_SIZE);
 		
 		// Stream the XML file. StAX is faster than DOM (waaaaaaaaaay faster) or SAX.
 		XMLInputFactory xif = XMLInputFactory.newInstance();
@@ -61,68 +62,94 @@ class ProcessUniprotXML
 		xsr.nextTag(); // Advance to statements element
 
 		// But... we're also going to use XSL for parts of the document!
-		TransformerFactory tf = TransformerFactory.newInstance();
 		// Load the XSL.
 		Source xsl = new StreamSource(new FileInputStream(PATH_TO_XSL));
-		Transformer transformer = tf.newTransformer(xsl);
+		Transformer transformer = TransformerFactory.newInstance().newTransformer(xsl);
 		// ...AND we'll use JAXB to take the transformed output and turn it into an object!
 		JAXBContext unmarshallerContext = JAXBContext.newInstance(org.reactome.release.uniprotupdate.dataschema.UniprotData.class);
-		JAXBContext marshallerContext = JAXBContext.newInstance(org.reactome.release.uniprotupdate.dataschema.UniprotData.class);
-		Marshaller marshaller = marshallerContext.createMarshaller();
-		marshaller.setProperty("jaxb.formatted.output", true);
-		marshaller.setProperty("jaxb.fragment", true);
-		
-		OutputStream debugOutputStream = null; 
-		
-		if (debugXML)
-		{
-			debugOutputStream = new FileOutputStream("simplified_uniprot_sprot.xml");
-		}
+
 		long startTime = System.currentTimeMillis();
 		while (xsr.nextTag() == XMLStreamConstants.START_ELEMENT)
 		{
 			// Check to see if we found "<entry>"
-			if (xsr.getName().getLocalPart().equals(ENTRY_ELEMENT_NAME))
+			if (foundEntry(xsr))
 			{
+				// Create a new StAX source based on the current stream reader, which is pointing at "<entry>",
+				// and then pass that to the transformer, and then unmarshall that into a Java class (UniprotData).
+				StAXSource src = new StAXSource(xsr);
+
+				JAXBResult result = new JAXBResult(unmarshallerContext);
+
 				try
 				{
-					// Create a new StAX source based on the current stream reader, which is pointing at "<entry>",
-					// and then pass that to the transformer, and then unmarshall that into a Java class (UniprotData).
-					StAXSource src = new StAXSource(xsr);
-					
-					JAXBResult result = new JAXBResult(unmarshallerContext);
 					transformer.transform(src, result);
-					// Try to also send to a file.
-					if (debugXML)
-					{
-						marshaller.marshal((UniprotData)result.getResult(), debugOutputStream);
-					}
-					
-					// Add the result to the list.
-					uniprotData.add((UniprotData)result.getResult());
 				}
-				// Hmmm... should I break the loop if something bad happens here? Or just print the stacktrace/some custom message, and then
-				// keep going? Hasn't happened yet with samples of REAL data.
 				catch (TransformerException e)
 				{
+					// Hmmm... should I break the loop if something bad happens here? Or just print the stacktrace/some custom message, and then
+					// keep going? Hasn't happened yet with samples of REAL data.
 					e.printStackTrace();
 				}
-				catch (JAXBException e)
+
+				// Try to also send to a file.
+				if (debugXML)
 				{
-					e.printStackTrace();
+					sendResultsToDebugFile(result);
 				}
+
+				// Add the result to the list.
+				uniprotData.add( (UniprotData) result.getResult() );
+
 				// Just to let the users know that progress *IS* being made.
 				if (uniprotData.size()%10000 == 0)
 				{
-					long endTime = System.currentTimeMillis();
-					
-					logger.info("{} records extracted in {} seconds.", uniprotData.size()   , Duration.ofMillis(endTime - startTime).toString() );
+					logProgressSince(startTime, uniprotData.size());
 				}
 			}
 		}
-		long endTime = System.currentTimeMillis();
-		logger.info("{} records extracted in {} seconds.", uniprotData.size(), Duration.ofMillis(endTime - startTime).toString() );
+		logProgressSince(startTime, uniprotData.size());
 		xsr.close();
 		return uniprotData;
+	}
+
+	private static boolean foundEntry(XMLStreamReader xsr)
+	{
+		return xsr.getName().getLocalPart().equals(ENTRY_ELEMENT_NAME);
+	}
+
+	private static void sendResultsToDebugFile(JAXBResult result)
+	{
+		try
+		{
+			JAXBContext marshallerContext = JAXBContext.newInstance(org.reactome.release.uniprotupdate.dataschema.UniprotData.class);
+			Marshaller marshaller = marshallerContext.createMarshaller();
+			marshaller.setProperty("jaxb.formatted.output", true);
+			marshaller.setProperty("jaxb.fragment", true);
+			marshaller.marshal(result.getResult(), getDebugOutputStream());
+		}
+		catch (JAXBException | FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static OutputStream getDebugOutputStream() throws FileNotFoundException
+	{
+		if (debugOutputStream == null)
+		{
+			debugOutputStream = new FileOutputStream("simplified_uniprot_sprot.xml");
+		}
+
+		return debugOutputStream;
+	}
+
+	private static void logProgressSince(long startTime, int dataListSize)
+	{
+		long duration = System.currentTimeMillis() - startTime;
+
+		logger.info("{} records extracted in {} seconds.",
+			dataListSize,
+			Duration.ofMillis(duration).toString()
+		);
 	}
 }

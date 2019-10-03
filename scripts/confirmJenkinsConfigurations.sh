d2ef5b52154190dba357fb4691b4e2f698179560
cookersjs <cookersjs@gmail.com>
2019-10-03 13:01:08 -0400

    Additional information messages for configuration checker

--- scripts/confirmJenkinsConfigurations.sh
+++ scripts/confirmJenkinsConfigurations.sh
@@ -22,18 +22,21 @@ done < $configFile
 
 if [[ $userInputReleaseNumber != $environmentalReleaseNumber ]] ; then #|| [[ $userInputReleaseNumber != $configFileReleaseNumber ]]; then
   echo "User input release number ($userInputReleaseNumber) does not match Jenkins environmental release number ($environmentalReleaseNumber)."
+  echo "Please update release-specific environmental variables found at 'Jenkins -> Manage Jenkins -> Configure System -> Global Properties'."
   exit 2
 fi
 
 if  [[ $userInputReleaseNumber != $configFileReleaseNumber ]]; then
   echo "User input release number ($userInputReleaseNumber) does not match config file release number ($configFileReleaseNumber)."
+  echo "Please update config file at 'Jenkins -> Releases -> $userInputReleaseNumber -> Credentials -> master_config.properties(Release-specific credentials)'."
   exit 2
 fi
 
 previousReleaseNumber="$(($userInputReleaseNumber-1))"
 
 if [[ $environmentalPrevReleaseNumber != $previousReleaseNumber ]]; then
-  echo "Jenkins environmental 'previous' release number ($environmentalPrevReleaseNumber) does not match expected previous release number ($previousReleaseNumber)"
+  echo "Jenkins environmental 'previous' release number ($environmentalPrevReleaseNumber) does not match expected previous release number ($previousReleaseNumber)."
+  echo "Please update release-specific environmental variables found at 'Jenkins -> Manage Jenkins -> Configure System -> Global Properties'."
   exit 2
 fi
 

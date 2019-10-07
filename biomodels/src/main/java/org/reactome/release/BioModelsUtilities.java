c6a1992a3e44973a00544cfc68c872fa951f6fa2
cookersjs <cookersjs@gmail.com>
2019-10-07 14:34:33 -0400

    Refactored BioModelsUtilitiesTest to make it more testable; Added tests

--- biomodels/src/main/java/org/reactome/release/BioModelsUtilities.java
+++ biomodels/src/main/java/org/reactome/release/BioModelsUtilities.java
@@ -6,65 +6,94 @@ import org.gk.model.GKInstance;
 import org.gk.model.ReactomeJavaConstants;
 import org.gk.persistence.MySQLAdaptor;
 import org.gk.schema.SchemaClass;
-import org.springframework.util.CollectionUtils;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 public class BioModelsUtilities {
 
     private static final Logger logger = LogManager.getLogger();
 
+    /**
+     * Attempts to find a 'BioModels Database' instance in the database. If there isn't one, it is created.
+     * @param dba -- MySQLAdaptor for database.
+     * @param instanceEdit -- GKInstance connecting user to modifications completed by this step.
+     * @return -- (GKInstance) The biomodels database instance
+     */
     public static GKInstance fetchBiomodelsReferenceDatabase(MySQLAdaptor dba, GKInstance instanceEdit) {
+        
         //TODO Need to make this retrieval case insensitive using a REGEX
-        GKInstance biomodelsReferenceDatabase = null;
         logger.info("Attempting to fetch an existing BioModels reference database");
-        try {
-           Collection<GKInstance> biomodelsReferenceDatabaseCollection = dba.fetchInstanceByAttribute(
-                   ReactomeJavaConstants.ReferenceDatabase,
-                   ReactomeJavaConstants.name,
-                   "=",
-                   "BioModels");
-           // TODO: Once merged into develop, this collection null check can be handled by 'safeList' in CollectionUtils
-           if (biomodelsReferenceDatabaseCollection != null && biomodelsReferenceDatabaseCollection.size()  != 0) {
-               biomodelsReferenceDatabase = biomodelsReferenceDatabaseCollection.iterator().next();
-           } else {
-               biomodelsReferenceDatabase = null;
-           }
-        } catch (Exception e) {
-            logAndThrow("Unable to retrieve BioModels reference database", e);
-        }
+        GKInstance biomodelsReferenceDatabase = retrieveBioModelsDatabaseInstance(dba);
 
         if (biomodelsReferenceDatabase == null) {
             logger.info("Creating BioModels reference database - no existing one was found");
-            biomodelsReferenceDatabase = new GKInstance();
-            biomodelsReferenceDatabase.setDbAdaptor(dba);
-            SchemaClass referenceDatabase = dba.getSchema().getClassByName(ReactomeJavaConstants.ReferenceDatabase);
-            biomodelsReferenceDatabase.setSchemaClass(referenceDatabase);
-
-            try {
-                biomodelsReferenceDatabase.addAttributeValue(ReactomeJavaConstants.created, instanceEdit);
-                String bioModelsDatabaseName = "BioModels Database";
-                biomodelsReferenceDatabase.addAttributeValue(
-                        ReactomeJavaConstants.name, Arrays.asList(bioModelsDatabaseName, "BioModels")
-                );
-                biomodelsReferenceDatabase.setDisplayName(bioModelsDatabaseName);
-
-                //TODO Check if both accessUrl and Url should be using https
-                biomodelsReferenceDatabase.addAttributeValue(
-                        ReactomeJavaConstants.accessUrl, "http://www.ebi.ac.uk/biomodels-main/publ-model.do?mid=###ID###"
-                );
-                biomodelsReferenceDatabase.addAttributeValue(
-                        ReactomeJavaConstants.url, "https://www.ebi.ac.uk/biomodels/"
-                );
-                dba.storeInstance(biomodelsReferenceDatabase);
-            } catch (Exception e) {
-                logger.error("Unable to create BioModels reference database", e);
-            }
-            logger.info("Successfully created BioModels reference database with db id of " +
-                    biomodelsReferenceDatabase.getDBID());
+            biomodelsReferenceDatabase = createBioModelsDatabaseInstance(dba, instanceEdit);
         }
+
+        return biomodelsReferenceDatabase;
+    }
+
+    /**
+     * Attempts to find the biomodels database instance.
+     * @param dba
+     * @return -- (GKInstance) The biomodels database instance, if it was found. Returns null if not.
+     */
+    public static GKInstance retrieveBioModelsDatabaseInstance(MySQLAdaptor dba) {
+
+        GKInstance biomodelsReferenceDatabase = null;
+        try {
+            @SuppressWarnings("unchecked")
+            Collection<GKInstance> biomodelsReferenceDatabaseCollection = dba.fetchInstanceByAttribute(
+                    ReactomeJavaConstants.ReferenceDatabase,
+                    ReactomeJavaConstants.name,
+                    "=",
+                    "BioModels");
+            // TODO: Once merged into develop, this collection null check can be handled by 'safeList' in CollectionUtils
+            if (biomodelsReferenceDatabaseCollection != null && biomodelsReferenceDatabaseCollection.size()  != 0) {
+                biomodelsReferenceDatabase = biomodelsReferenceDatabaseCollection.iterator().next();
+            }
+        } catch (Exception e) {
+            logAndThrow("Unable to retrieve BioModels reference database", e);
+        }
+        return biomodelsReferenceDatabase;
+    }
+
+    /**
+     * Creates biomodels database instance.
+     * @param dba
+     * @param instanceEdit
+     * @return -- (GKInstance) The biomodels database instance
+     */
+    private static GKInstance createBioModelsDatabaseInstance(MySQLAdaptor dba, GKInstance instanceEdit) {
+
+        GKInstance biomodelsReferenceDatabase;
+        biomodelsReferenceDatabase = new GKInstance();
+        biomodelsReferenceDatabase.setDbAdaptor(dba);
+        SchemaClass referenceDatabase = dba.getSchema().getClassByName(ReactomeJavaConstants.ReferenceDatabase);
+        biomodelsReferenceDatabase.setSchemaClass(referenceDatabase);
+
+        try {
+            biomodelsReferenceDatabase.addAttributeValue(ReactomeJavaConstants.created, instanceEdit);
+            String bioModelsDatabaseName = "BioModels Database";
+            biomodelsReferenceDatabase.addAttributeValue(
+                    ReactomeJavaConstants.name, Arrays.asList(bioModelsDatabaseName, "BioModels")
+            );
+            biomodelsReferenceDatabase.setDisplayName(bioModelsDatabaseName);
+
+            //TODO Check if both accessUrl and Url should be using https
+            biomodelsReferenceDatabase.addAttributeValue(
+                    ReactomeJavaConstants.accessUrl, "http://www.ebi.ac.uk/biomodels-main/publ-model.do?mid=###ID###"
+            );
+            biomodelsReferenceDatabase.addAttributeValue(
+                    ReactomeJavaConstants.url, "https://www.ebi.ac.uk/biomodels/"
+            );
+            dba.storeInstance(biomodelsReferenceDatabase);
+        } catch (Exception e) {
+            logger.error("Unable to create BioModels reference database", e);
+        }
+        logger.info("Successfully created BioModels reference database with db id of " +
+                biomodelsReferenceDatabase.getDBID());
 
         return biomodelsReferenceDatabase;
     }
---
+++ biomodels/src/test/java/org/reactome/release/BioModelsUtilitiesTester.java
@@ -0,0 +1,52 @@
+package org.reactome.release;
+
+import org.gk.model.GKInstance;
+import org.gk.persistence.MySQLAdaptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({BioModelsUtilities.class})
+@PowerMockIgnore({"org.apache.logging.log4j.*", "javax.management.*", "javax.script.*",
+        "javax.xml.*", "com.sun.org.apache.xerces.*", "org.xml.sax.*", "com.sun.xml.*", "org.w3c.dom.*"})
+public class BioModelsUtilitiesTester {
+
+    @Mock
+    private MySQLAdaptor mockAdaptor;
+    @Mock
+    private GKInstance mockBiomodelsDatabase;
+
+    @Test
+    public void bioModelsReferenceDatabaseExistsInDBReturnsInstance() throws Exception {
+
+        Collection<GKInstance> collectionWithMockDatabaseInstance = new ArrayList<>();
+        collectionWithMockDatabaseInstance.add(mockBiomodelsDatabase);
+
+        Mockito.when(mockAdaptor.fetchInstanceByAttribute("ReferenceDatabase", "name", "=", "BioModels")).thenReturn(collectionWithMockDatabaseInstance);
+        GKInstance returnedDbInstance = BioModelsUtilities.retrieveBioModelsDatabaseInstance(mockAdaptor);
+        assertThat(returnedDbInstance, is(equalTo(mockBiomodelsDatabase)));
+    }
+
+    @Test
+    public void bioModelsReferenceDatabaseDoesNotExistInDBReturnsNull() throws Exception {
+
+        Collection<GKInstance> collectionWithoutMockDatabaseInstance = new ArrayList<>();
+
+        Mockito.when(mockAdaptor.fetchInstanceByAttribute("ReferenceDatabase", "name", "=", "BioModels")).thenReturn(collectionWithoutMockDatabaseInstance);
+        GKInstance returnedDbInstance = BioModelsUtilities.retrieveBioModelsDatabaseInstance(mockAdaptor);
+        assertThat(returnedDbInstance, is(equalTo(null)));
+    }
+
+}

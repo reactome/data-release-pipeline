b6449ad92679d9ebbcbb3eeac5b6547383285092
cookersjs <cookersjs@gmail.com>
2019-10-25 10:39:16 -0400

    Refactor of ModelsTSVParser test class

--- biomodels/src/test/java/org/reactome/release/ModelsTSVParserTester.java
+++ biomodels/src/test/java/org/reactome/release/ModelsTSVParserTester.java
@@ -2,61 +2,88 @@ package org.reactome.release;
 
 import org.junit.Test;
 
+import java.nio.file.Paths;
 import java.util.*;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class ModelsTSVParserTester {
 
     private final int correctPathwayCount = 3;
     private final int correctNumberPathwaysInTestFile = 745;
-    private final int correctNumberBioModelsIdsInTestFile = 153;
+    private final long correctNumberBioModelsIdsInTestFile = 153L;
+    private final String RESOURCE_DIR = Paths.get("src", "test", "resources").toFile().getAbsolutePath();
+
     @Test
-    public void emptyMapFromEmptyFileName() {
+    public void emptyFilenameReturnsEmptyMap() {
         Map<String, List<String>> pathwayToBioModelsIds =  ModelsTSVParser.parse("");
-        assertTrue("Empty file produces empty pathwayToBioModelsIds map", pathwayToBioModelsIds.isEmpty());
+        assertThat("Empty file produces empty pathwayToBioModelsIds map", pathwayToBioModelsIds.isEmpty(), is(true));
     }
 
     @Test
-    public void emptyMapFromUnknownFileName() {
+    public void unknownFileNameReturnsEmptyMap() {
+        String filepath = Paths.get(RESOURCE_DIR, "filedoesnotexist.tsv").toFile().getAbsolutePath();
         Map<String, List<String>> pathwayToBioModelsIds =  ModelsTSVParser.parse("filedoesnotexist.tsv");
-        assertTrue("Unknown file produces empty pathwayToBioModelsIds map", pathwayToBioModelsIds.isEmpty());
+        assertThat("Unknown file produces empty pathwayToBioModelsIds map", pathwayToBioModelsIds.isEmpty(), is(true));
     }
 
     @Test
-    public void emptyMapFromImproperFile() {
-        Map<String, List<String>> pathwayToBioModelsIds = ModelsTSVParser.parse("src/test/resources/improper_models2pathways.tsv");
-        assertTrue("Improperly formatted file produces empty pathwayToBioModelsIds map", pathwayToBioModelsIds.isEmpty());
+    public void improperlyFormattedFileReturnsEmptyMap() {
+        String filepath = Paths.get(RESOURCE_DIR, "improper_models2pathways.tsv").toFile().getAbsolutePath();
+        Map<String, List<String>> pathwayToBioModelsIds = ModelsTSVParser.parse(filepath);
+        assertThat("Improperly formatted file produces empty pathwayToBioModelsIds map", pathwayToBioModelsIds.isEmpty(), is(true));
     }
 
     @Test
-    public void populatedMapFromPartiallyCorrectFile() {
-        Map<String, List<String>> pathwayToBioModelsIds = ModelsTSVParser.parse("src/test/resources/partiallycorrect_models2pathways.tsv");
-        assertEquals(pathwayToBioModelsIds.keySet().size(), correctPathwayCount);
-        assertTrue(pathwayToBioModelsIds.get("R-MMU-1169091").contains("BIOMD0000000226"));
+    public void partiallyCorrectFileReturnsPopulatedMap() {
+        String filepath = Paths.get(RESOURCE_DIR, "partiallycorrect_models2pathways.tsv").toFile().getAbsolutePath();
+        Map<String, List<String>> pathwayToBioModelsIds = ModelsTSVParser.parse(filepath);
+        assertThat(pathwayToBioModelsIds.keySet().size(), is(equalTo(correctPathwayCount)));
+        assertThat(pathwayToBioModelsIds.get("R-MMU-1169091").contains("BIOMD0000000226"), is(equalTo(true)));
     }
 
     @Test
-    public void testModels2PathwaysCorrectlyParsed() {
-        Map<String, List<String>> pathwayToBioModelsIds =
-            ModelsTSVParser.parse("src/test/resources/models2pathways.tsv");
-        assertEquals(pathwayToBioModelsIds.keySet().size(), correctNumberPathwaysInTestFile);
+    public void testFileReturnsPopulatedMap() {
+        String filepath = Paths.get(RESOURCE_DIR, "models2pathways.tsv").toFile().getAbsolutePath();
+        Map<String, List<String>> pathwayToBioModelsIds = ModelsTSVParser.parse(filepath);
+        assertThat(pathwayToBioModelsIds.keySet().size(), is(equalTo(correctNumberPathwaysInTestFile)));
+    }
 
+    @Test
+    public void testFileReturnsCorrectNumberOfIds() {
+        String filepath = Paths.get(RESOURCE_DIR, "models2pathways.tsv").toFile().getAbsolutePath();
+        Map<String, List<String>> pathwayToBioModelsIds = ModelsTSVParser.parse(filepath);
         long bioModelsIdsCount =
-            pathwayToBioModelsIds.keySet().stream().map(
-                pathway -> pathwayToBioModelsIds.get(pathway)
-            ).flatMap(
-                Collection::stream
-            ).distinct().count();
-        assertEquals(bioModelsIdsCount, correctNumberBioModelsIdsInTestFile); // Number of distinct BioModels Ids in test input file
+                pathwayToBioModelsIds.keySet().stream().map(
+                        pathway -> pathwayToBioModelsIds.get(pathway)
+                ).flatMap(
+                        Collection::stream
+                ).distinct().count();
+        assertThat(bioModelsIdsCount, is(equalTo(correctNumberBioModelsIdsInTestFile))); // Number of distinct BioModels Ids in test input file
+    }
 
+    @Test
+    public void testFileContainsCorrectMapping() {
+        String filepath = Paths.get(RESOURCE_DIR, "models2pathways.tsv").toFile().getAbsolutePath();
+        Map<String, List<String>> pathwayToBioModelsIds = ModelsTSVParser.parse(filepath);
         // Tests pathway contains correct BioModels id
-        assertTrue(pathwayToBioModelsIds.get("R-HSA-199418").contains("BIOMD0000000175"));
+        assertThat(pathwayToBioModelsIds.get("R-HSA-199418").contains("BIOMD0000000175"), is(equalTo(true)));
+    }
 
+    @Test
+    public void testFileProducesMappingWithoutNullKeysOrValues() {
+        String filepath = Paths.get(RESOURCE_DIR, "models2pathways.tsv").toFile().getAbsolutePath();
+        Map<String, List<String>> pathwayToBioModelsIds = ModelsTSVParser.parse(filepath);
         // Tests for null in Map keys and value
-        assertTrue("pathwayToBioModelsIds map does not contain any null keys", !pathwayToBioModelsIds.containsKey(null));
-        assertTrue("pathwayToBioModelsIds map does not contain any null values", !pathwayToBioModelsIds.containsValue(null));
+        assertThat("pathwayToBioModelsIds map does not contain any null keys", pathwayToBioModelsIds.containsKey(null), is(equalTo(false)));
+        assertThat("pathwayToBioModelsIds map does not contain any null values", pathwayToBioModelsIds.containsValue(null), is(equalTo(false)));
+    }
 
+    @Test
+    public void testFileProducesMappingWithoutListContainingNullValues() {
+        String filepath = Paths.get(RESOURCE_DIR, "models2pathways.tsv").toFile().getAbsolutePath();
+        Map<String, List<String>> pathwayToBioModelsIds = ModelsTSVParser.parse(filepath);
         // Tests for null in any List object in pathwayToBioModelsIds Map
         boolean nullValueInList = false;
         for (String pathwayToBioModelsIdsKey : pathwayToBioModelsIds.keySet()) {
@@ -64,6 +91,6 @@ public class ModelsTSVParserTester {
                 nullValueInList = true;
             }
         }
-        assertTrue("No null values in any lists in pathwaysToBioModelsIds map", !nullValueInList);
+        assertThat("No null values in any lists in pathwaysToBioModelsIds map", nullValueInList, is(equalTo(false)));
     }
 }

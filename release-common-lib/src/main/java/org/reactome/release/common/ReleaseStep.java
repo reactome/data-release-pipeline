aa090aa9e4eb951149f255c66f1c6f31a9717258
Joel Weiser <joel.weiser@oicr.on.ca>
2019-10-31 13:03:12 -0400

    refactors ReleaseStep class

--- release-common-lib/src/main/java/org/reactome/release/common/ReleaseStep.java
+++ release-common-lib/src/main/java/org/reactome/release/common/ReleaseStep.java
@@ -1,7 +1,5 @@
 package org.reactome.release.common;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -9,37 +7,30 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.gk.persistence.MySQLAdaptor;
 
-public abstract class ReleaseStep
-{
+public abstract class ReleaseStep {
 	protected static final Logger logger = LogManager.getLogger();
-	
+
 	protected boolean testMode;
-	
-	protected static MySQLAdaptor getMySQLAdaptorFromProperties(Properties props) throws IOException, FileNotFoundException, SQLException
-	{
-		
+
+	protected static MySQLAdaptor getMySQLAdaptorFromProperties(Properties props) throws SQLException {
+
 		String dbHost = props.getProperty("db.host", "localhost");
 		String dbUser = props.getProperty("db.user");
 		String dbPassword = props.getProperty("db.password");
 		String dbName = props.getProperty("db.name");
 		int dbPort = new Integer(props.getProperty("db.port", "3306"));
-		
-		MySQLAdaptor adaptor = new MySQLAdaptor(dbHost, dbName, dbUser, dbPassword, dbPort);
-		return adaptor;
+
+		return new MySQLAdaptor(dbHost, dbName, dbUser, dbPassword, dbPort);
 	}
-	
-	protected void loadTestModeFromProperties(Properties props)
-	{
+
+	protected void setTestModeFromProperties(Properties props) {
 		this.testMode = new Boolean(props.getProperty("testMode", "true"));
-		if (!testMode)
-		{
+		if (!testMode) {
 			logger.info("Test mode is OFF - database will be updated!");
-		}
-		else
-		{
+		} else {
 			logger.info("Test mode is ON - no database changes will be made.");
 		}
 	}
-	
-	abstract public void executeStep(Properties props) throws Exception; 
+
+	abstract public void executeStep(Properties props) throws Exception;
 }

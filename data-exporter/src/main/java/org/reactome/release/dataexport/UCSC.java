1022e5ec7c14c5a6646f91f58c5f4dd8edbe03f2
Joel Weiser <joel.weiser@oicr.on.ca>
2019-11-11 18:08:19 -0500

    removes unneeded import from UCSC class file

--- data-exporter/src/main/java/org/reactome/release/dataexport/UCSC.java
+++ data-exporter/src/main/java/org/reactome/release/dataexport/UCSC.java
@@ -1,6 +1,5 @@
 package org.reactome.release.dataexport;
 
-import java.util.stream.Stream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.neo4j.driver.v1.*;

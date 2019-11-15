b4d5a9e136d0c77c099e44db59ce5b10d784077a
Joel Weiser <joel.weiser@oicr.on.ca>
2019-11-14 21:12:31 -0500

    refactors and adds JavaDocs to the Keyword class

--- uniprot-update/src/main/java/org/reactome/release/uniprotupdate/dataschema/Keyword.java
+++ uniprot-update/src/main/java/org/reactome/release/uniprotupdate/dataschema/Keyword.java
@@ -4,32 +4,54 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.XmlValue;
 
+/**
+ * Class representing a Keyword of a UniProt entry
+ * @author sshorser
+ */
 @XmlType(name="keyword")
-public class Keyword
-{
+public class Keyword {
 	private String id;
-	
 	private String keyword;
 
+	/**
+	 * Returns the id of the Keyword
+	 * @return The id of the Keyword or an empty String if none
+	 */
 	@XmlAttribute(name="id")
-	public String getId()
-	{
+	public String getId() {
+		if (id == null) {
+			return "";
+		}
+
 		return id;
 	}
-	
-	public void setId(String id)
-	{
+
+	/**
+	 * Sets the id of the Keyword
+	 * @param id The id of the Keyword
+	 */
+	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * Returns the Keyword text value
+	 * @return The Keyword text value or an empty String if none
+	 */
 	@XmlValue
-	public String getKeyword()
-	{
+	public String getKeyword() {
+		if (keyword == null) {
+			return "";
+		}
+
 		return keyword;
 	}
-	
-	public void setKeyword(String value)
-	{
+
+	/**
+	 * Sets the text value of the Keyword
+	 * @param value The text value of the Keyword
+	 */
+	public void setKeyword(String value) {
 		this.keyword = value;
 	}
 }

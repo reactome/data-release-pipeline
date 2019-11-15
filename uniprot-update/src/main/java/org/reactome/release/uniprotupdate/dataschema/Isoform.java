84fb539d5f6a27e07b5524197a4f935b7531a485
Joel Weiser <joel.weiser@oicr.on.ca>
2019-11-14 21:12:57 -0500

    refactors and adds JavaDocs to the Isoform class

--- uniprot-update/src/main/java/org/reactome/release/uniprotupdate/dataschema/Isoform.java
+++ uniprot-update/src/main/java/org/reactome/release/uniprotupdate/dataschema/Isoform.java
@@ -3,20 +3,33 @@ package org.reactome.release.uniprotupdate.dataschema;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 
+/**
+ * Class representing an Isoform of a UniProt entry
+ * @author sshorser
+ */
 @XmlType(name="isoform")
-public class Isoform
-{
+public class Isoform {
 	private String isoformID;
 
+	/**
+	 * Returns the identifier of the Isoform
+	 * @return The identifier Isoform or an empty String if none
+	 */
 	@XmlAttribute(name="id")
-	public String getIsoformID()
-	{
+	public String getIsoformID() {
+		if (isoformID == null) {
+			return "";
+		}
+
 		return isoformID;
 	}
 
-	public void setIsoformID(String isoformID)
-	{
+	/**
+	 * Sets the identifier of the Isoform
+	 * @param isoformID The identifier Isoform
+	 */
+	public void setIsoformID(String isoformID) {
 		this.isoformID = isoformID;
 	}
-	
+
 }

b2c1ebc0a775fb42fdad27e844fdc8458ff4cb17
Joel Weiser <joel.weiser@oicr.on.ca>
2019-11-14 21:13:16 -0500

    refactors and adds JavaDocs to the Gene class

--- uniprot-update/src/main/java/org/reactome/release/uniprotupdate/dataschema/Gene.java
+++ uniprot-update/src/main/java/org/reactome/release/uniprotupdate/dataschema/Gene.java
@@ -1,25 +1,39 @@
 package org.reactome.release.uniprotupdate.dataschema;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
+/**
+ * Class representing a Gene which encodes a UniProt entry
+ * @author sshorser
+ */
 @XmlType(name="gene")
-public class Gene
-{
-	private List<Name> names;
+public class Gene {
+	private List<GeneName> names;
 
+	/**
+	 * Returns the list of names associated with this Gene object
+	 * @return List of GeneName objects or an empty list if there are none
+	 */
 	@XmlElement(name="name")
-	public List<Name> getNames()
-	{
+	public List<GeneName> getNames() {
+		if (names == null) {
+			return new ArrayList<>();
+		}
+
 		return names;
 	}
 
-	public void setNames(List<Name> names)
-	{
+	/**
+	 * Sets the list of names associated with this Gene object
+	 * @param names List of GeneName objects
+	 */
+	public void setNames(List<GeneName> names) {
 		this.names = names;
 	}
-	
+
 }

b21adbba0b5d55819b9041d970911a6052c1295d
cookersjs <cookersjs@gmail.com>
2019-11-19 09:57:22 -0500

    Added instance edit for modifications

--- orthoinference/src/main/java/org/reactome/datafix/KEGGAddLinksMycobacteriumH37RvLinkFix.java
+++ orthoinference/src/main/java/org/reactome/datafix/KEGGAddLinksMycobacteriumH37RvLinkFix.java
@@ -3,6 +3,7 @@ package org.reactome.datafix;
 import org.gk.model.GKInstance;
 import org.gk.model.ReactomeJavaConstants;
 import org.gk.persistence.MySQLAdaptor;
+import org.reactome.release.common.database.InstanceEditUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -21,10 +22,15 @@ public class KEGGAddLinksMycobacteriumH37RvLinkFix {
         String database = props.getProperty("release_current.name");
         String host = props.getProperty("release.database.host");
         int port = Integer.valueOf(props.getProperty("release.database.port"));
+        long personId = Long.valueOf(props.getProperty("personId"));
 
         MySQLAdaptor dba = new MySQLAdaptor(host, database, username, password, port);
 
         Map<String, String> identifierToPrefixMap = new HashMap<>();
+
+        GKInstance instanceEditInst = InstanceEditUtils.createInstanceEdit(dba, personId, "org.reactome.release");
+        instanceEditInst.addAttributeValue(ReactomeJavaConstants.note, "In release 71, changed accessUrl in 'KEGG Gene M. tuberculosis H37Rv' refDB and the corresponding identifier in its referrals");
+        dba.updateInstance(instanceEditInst);
 
         // mtv-mappings.txt is the result of grepping all uniprot files for 'mtv:'. Same was done for 'mtu:'.
         // Each of these lines (there are 178) maps to one of the identifiers we want to update.
@@ -55,8 +61,7 @@ public class KEGGAddLinksMycobacteriumH37RvLinkFix {
             }
         }
 
-        //TODO: Should display name be updated? Technically the identifier includes the prefix, but from Reactome's perspective we've never included the prefix.
-        //TODO: Add instanceEdit for these changes.
+        //TODO: Should display name be updated? Technically the identifier includes the prefix, but from Reactome's perspective we've never included the prefix for any of the KEGG identifiers.
 
         // Now with the map, I get the reference database of interest. Then I get all referrals of it, which corresponds to the 178 DatabaseIdentifiers.
         GKInstance keggMycoDBInst = (GKInstance) dba.fetchInstanceByAttribute(ReactomeJavaConstants.ReferenceDatabase, ReactomeJavaConstants.name, "=", "KEGG Gene (Mycobacterium tuberculosis H37Rv)").iterator().next();
@@ -67,7 +72,10 @@ public class KEGGAddLinksMycobacteriumH37RvLinkFix {
             if (identifierToPrefixMap.get(identifier) !=null) {
                 // DatabaseIdentifier has its 'identifier' attribute updated to include the prefix. Note: The displayName will remain unchanged.
                 keggMycoRefferal.setAttributeValue(ReactomeJavaConstants.identifier, identifierToPrefixMap.get(identifier));
+                keggMycoRefferal.getAttributeValue(ReactomeJavaConstants.modified);
+                keggMycoRefferal.addAttributeValue(ReactomeJavaConstants.modified, instanceEditInst);
                 dba.updateInstanceAttribute(keggMycoRefferal, ReactomeJavaConstants.identifier);
+                dba.updateInstanceAttribute(keggMycoRefferal, ReactomeJavaConstants.modified);
             } else {
                 System.out.println("THIS IS NOT A DRILL!");
             }
@@ -77,7 +85,10 @@ public class KEGGAddLinksMycobacteriumH37RvLinkFix {
         String accessUrl = keggMycoDBInst.getAttributeValue(ReactomeJavaConstants.accessUrl).toString();
         accessUrl = accessUrl.replace("mtv:", "");
         keggMycoDBInst.setAttributeValue(ReactomeJavaConstants.accessUrl, accessUrl);
+        keggMycoDBInst.getAttributeValue(ReactomeJavaConstants.modified);
+        keggMycoDBInst.addAttributeValue(ReactomeJavaConstants.modified, instanceEditInst);
         dba.updateInstanceAttribute(keggMycoDBInst, ReactomeJavaConstants.accessUrl);
+        dba.updateInstanceAttribute(keggMycoDBInst, ReactomeJavaConstants.modified);
 
         System.out.println("Finished updating accessURL and identifiers associated with M. tuberculosis H37Rv");
 
